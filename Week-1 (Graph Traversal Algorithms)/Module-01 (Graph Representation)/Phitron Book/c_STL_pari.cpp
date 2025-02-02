4a87f07efe9cbc241ab8344b0c1e2a7e3c86873b
GitGuru420 <raisulislam1892@gmail.com>
2025-02-02 13:35:53 +0600

    STL pair

---
+++ Week-1 (Graph Traversal Algorithms)/Module-01 (Graph Representation)/Phitron Book/c_STL_pari.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // pair<int, string> p1;
+    // p1 = make_pair(100, "Algorithm");
+
+    // pair<int, string> p1(2, "Algorithm");
+    
+    pair<int, string> p1;
+    p1 = {101, "Algorithm"};
+    cout << p1.first << " -> " << p1.second << endl;
+    return 0;
+}
