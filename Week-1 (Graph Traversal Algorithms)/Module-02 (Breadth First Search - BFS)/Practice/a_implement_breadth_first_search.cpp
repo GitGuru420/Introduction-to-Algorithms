da313fa5b34792c5197004c170a21fba0a419852
GitGuru420 <raisulislam1892@gmail.com>
2025-02-03 21:57:15 +0600

    Implement Breadht First Search

---
+++ Week-1 (Graph Traversal Algorithms)/Module-02 (Breadth First Search - BFS)/Practice/a_implement_breadth_first_search.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> adj_list[1000];
+bool visited_node[1000];
+
+void breadth_first_search(int source) {
+    queue<int> q;
+    q.push(source);
+    visited_node[source] = true;
+    while(!q.empty()) {
+        int parent = q.front();
+        q.pop();
+
+        cout << parent << " ";
+
+        for(int child : adj_list[parent]) {
+            if(!visited_node[child]) {
+                q.push(child);
+                visited_node[child] = true;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int node, edge; cin >> node >> edge;
+    while(edge--) {
+        int a, b;   cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+
+    memset(visited_node, false, sizeof(visited_node));
+    breadth_first_search(0);    // let source node --> 0
+
+    return 0;
+}
