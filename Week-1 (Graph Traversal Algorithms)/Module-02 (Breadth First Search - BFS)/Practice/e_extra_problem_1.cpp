b8d0a6923b1972255538a28a8ceaad11983b46cb
GitGuru420 <raisulislam1892@gmail.com>
2025-02-04 12:44:16 +0600

    Find distance from soruce to destination

---
+++ Week-1 (Graph Traversal Algorithms)/Module-02 (Breadth First Search - BFS)/Practice/e_extra_problem_1.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> adj_list[1000];
+int visited_node[1000];
+int level[1000];
+
+void breadth_first_search(int source) {
+    queue<int> q;
+    q.push(source);
+    visited_node[source] = true;
+    level[source] = 0;
+    while(!q.empty()) {
+        int parent = q.front();
+        q.pop();
+
+        for(int child : adj_list[parent]) {
+            if(!visited_node[child]) {
+                q.push(child);
+                visited_node[child] = true;
+                level[child] = level[parent] + 1;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int node, edge;     cin >> node >> edge;
+    while(edge--) {
+        int a, b;   cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+    memset(visited_node, false, sizeof(visited_node));
+    memset(level, -1, sizeof(level));
+    int source, destination;    cin >> source >> destination;
+    breadth_first_search(source);
+    cout << level[destination] << endl;
+
+    return 0;
+}
