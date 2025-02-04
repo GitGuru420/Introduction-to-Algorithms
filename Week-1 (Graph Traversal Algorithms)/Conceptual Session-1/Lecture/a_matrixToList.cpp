56946fd61b4e76e11d57605daa61194ffb3e63d3
GitGuru420 <raisulislam1892@gmail.com>
2025-02-04 21:29:23 +0600

    Convert adj_mat to adj_list

---
+++ Week-1 (Graph Traversal Algorithms)/Conceptual Session-1/Lecture/a_matrixToList.cpp
@@ -0,0 +1,59 @@
+/*
+Problem 1: Convert Adjacency Matrix to Adjacency List
+
+Problem Statement:
+Given an n × n adjacency matrix representing an unweighted graph, convert it into an adjacency list representation.
+
+Example:
+
+Input:
+3
+0 1 1
+1 0 0
+1 0 0
+
+Output:
+0 -> 1 2
+1 -> 0
+2 -> 0
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void matrixToList(int mat[][100], int n) {  // parameter format evave
+    vector<int> adjList[n];
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            if(mat[i][j] == 1) {
+                adjList[i].push_back(j);
+            }
+        }
+    }
+
+    for(int i=0; i<n; i++) {
+        cout << i << " -> ";
+        // for(int j=0; j<adjList[i].size(); i++) {
+        //     cout << adjList[i][j] << " ";
+        // }
+        for(int val : adjList[i]) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;  cin >> n;
+    int mat[100][100];
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cin >> mat[i][j];
+        }
+    }
+
+    matrixToList(mat, n);
+    return 0;
+}
