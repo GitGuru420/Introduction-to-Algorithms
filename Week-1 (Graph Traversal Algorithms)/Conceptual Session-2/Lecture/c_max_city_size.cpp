a43c9d33a9ee4f41606cb03a6870208c3c61b85d
GitGuru420 <raisulislam1892@gmail.com>
2025-02-06 21:33:10 +0600

    Max city size

---
+++ Week-1 (Graph Traversal Algorithms)/Conceptual Session-2/Lecture/c_max_city_size.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+char graph[1001][1001];
+bool vis[1001][1001];
+vector<pair<int,int>> direction = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+int row, col;
+int cnt;
+
+bool valid(int x, int y) {
+    if(x<0 || x>=row || y<0 || y>=col)  
+        return false;
+    else
+        return false;
+}
+void dfs(int sr, int sc) {
+    vis[sr][sc] = true;
+    cnt++;
+
+    for(int i=0; i<4; i++) {
+        int child_row = sr + direction[i].first;
+        int child_column = sc + direction[i].second; 
+
+        if(valid(child_row, child_column) && !vis[child_row][child_column] && graph[child_row][child_column] != '.')
+            dfs(child_row, child_column);
+    }
+}
+int main()
+{
+    cin >> row >> col;
+    for(int i=0; i<row; i++) {
+        for(int j=0; j<col; j++) {
+            cin >> graph[i][j];
+        }
+    }
+    memset(vis, false, sizeof(vis));
+    int max_size = 0;
+    for(int i=0; i<row; i++) {
+        for(int j=0; j<col; j++) {
+            if(graph[i][j] == '.') {
+                if(!vis[i][j]) {
+                    cnt = 0;    // resetting
+                    dfs(i, j);
+                    // cout << cnt << endl;
+                    if(max_size < cnt) {
+                        max_size = cnt;
+                    }
+                }
+            }
+        }
+    }
+    cout << max_size << endl;
+
+    return 0;
+}
