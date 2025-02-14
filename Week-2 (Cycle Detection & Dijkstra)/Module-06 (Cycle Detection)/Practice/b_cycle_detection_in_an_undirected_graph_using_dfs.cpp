b921cd4c420427b09ebe40391c4f7e791c1fe51b
GitGuru420 <raisulislam1892@gmail.com>
2025-02-14 14:46:53 +0600

    Detect cycle in an undirected graph using dfs

---
+++ Week-2 (Cycle Detection & Dijkstra)/Module-06 (Cycle Detection)/Practice/b_cycle_detection_in_an_undirected_graph_using_dfs.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> adjList[100];
+bool vis[100];
+int parent[100];
+bool cycle;
+
+void dfs(int src) {
+    vis[src] = true;
+    for(int child : adjList[src]) {
+        if(vis[child] && parent[src] != child) {
+            cycle = true;
+        }
+        if(!vis[child]) {
+            parent[child] = src;
+            dfs(child);
+        }
+    }
+}
+
+int main()
+{
+    int n, e;   cin >> n >> e;
+    while(e--) {
+        int a, b;   cin >> a >> b;
+        adjList[a].push_back(b); 
+        adjList[b].push_back(a); 
+    }
+    memset(vis, false, sizeof(vis));
+    memset(parent, -1, sizeof(parent));
+    cycle = false;
+
+    for(int i=0; i<n; i++) {
+        if(!vis[i]) {
+            dfs(i);
+        }
+    }
+
+    if(cycle)
+        cout << "Detected Cycle" << endl;
+    else
+        cout << "No, Cycle" << endl;
+
+    return 0;
+}
