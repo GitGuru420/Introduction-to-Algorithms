aeb35552c44935aa80493c3dad88db5e72048701
GitGuru420 <raisulislam1892@gmail.com>
2025-03-07 15:08:42 +0600

    Recursion

--- Week-4 (Dynamic Programming)/Module-14 (Basic of Dynamic Programming)/Lecture/a_recursion.cpp
+++ Week-4 (Dynamic Programming)/Module-14 (Basic of Dynamic Programming)/Lecture/a_recursion.cpp
@@ -1,24 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void rec1(int n) {
+void rec1(int n) {  // O(N)
     if(n>5)
         return;
     cout << n << " ";
     rec1(n+1);
 }
 
-void rec2(int n) {
+void rec2(int n) {  // O(N)
     if(n>5)
         return;
     rec2(n+1);
     cout << n << " ";
 }
 
+int rec3(int n) {  // O(N)
+    if(n>5) 
+        return 0;
+    int sum = rec3(n+1);
+    return sum + n;
+}
+
 int main()
 {
-    rec1(1);
-    cout << endl;
-    rec2(1);
+    // rec1(1);
+    // cout << endl;
+    // rec2(1);
+    cout << rec3(1) << endl;
     return 0;
 }
